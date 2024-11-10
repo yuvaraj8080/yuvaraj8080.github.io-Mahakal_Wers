26583dd767a077ab553a19a1a8e955f87e43ac9f
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-11-10 12:40:32 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3924784357"
+    serviceWorkerVersion: "3904276543"
   }
 });
